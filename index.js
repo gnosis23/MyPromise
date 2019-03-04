4b7e3627518343e079a9be5c416713db3078d12a
gnosis23 <bj050323@gmail.com>
2019-03-04 20:11:55 +0800

    es5

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-const PENDING = 'pending'
-const RESOLVED = 'resolved'
-const REJECTED = 'rejected'
+var PENDING = 'pending'
+var RESOLVED = 'resolved'
+var REJECTED = 'rejected'
 
 function nextMacroTask(fn) {
-  setTimeout(() => {
-    fn()
-  }, 0)
+  setTimeout(fn, 0)
 }
 
 function MyPromise(fn) {
-  const that = this
+  var that = this
   that.state = PENDING
   that.value = null
   that.resolvedCb = []
@@ -19,24 +17,24 @@ function MyPromise(fn) {
     if (value instanceof MyPromise) {
       return value.then(resolve, reject)
     }
-    nextMacroTask(() => {
+    nextMacroTask(function() {
       if (that.state === PENDING) {
         that.state = RESOLVED
         that.value = value
         that.resolvedCb.forEach(
-          cb => cb.call(null, that.value)
+          function(cb) { cb.call(null, that.value) }
         )
       }
     })
   }
 
   function reject(value) {
-    nextMacroTask(() => {
+    nextMacroTask(function() {
       if (that.state === PENDING) {
         that.state = REJECTED
         that.value = value
         that.rejectedCb.forEach(
-          cb => cb.call(null, that.value)
+          function(cb) { cb.call(null, that.value) }
         )
       }
     })
@@ -61,19 +59,19 @@ function resolutionProcedure(promise2, x, resolve, reject) {
     return
   }
 
-  let called = false
+  var called = false
   if (x !== null && (typeof x === 'object' || typeof x === 'function')) {
     try {
-      let then = x.then
+      var then = x.then
       if (typeof then === 'function') {
         then.call(
           x,
-          y => {
+          function(y) {
             if (called) return
             called = true
             resolutionProcedure(promise2, y, resolve, reject)
           },
-          e => {
+          function(e) {
             if (called) return
             called = true
             reject(e)
@@ -92,30 +90,30 @@ function resolutionProcedure(promise2, x, resolve, reject) {
 }
 
 MyPromise.prototype.then = function (onFulfilled, onRejected) {
-  const that = this
+  var that = this
   var promise2
 
   onFulfilled = typeof onFulfilled === 'function'
     ? onFulfilled
-    : v => v
+    : function(v) { return v }
   onRejected = typeof onRejected === 'function'
     ? onRejected
-    : r => { throw r }
+    : function(r) { throw r }
 
   if (that.state === PENDING) {
-    return (promise2 = new MyPromise((resolve, reject) => {
-      that.resolvedCb.push(() => {
+    return (promise2 = new MyPromise(function(resolve, reject) {
+      that.resolvedCb.push(function() {
         try {
-          const x = onFulfilled(that.value)
+          var x = onFulfilled(that.value)
           resolutionProcedure(promise2, x, resolve, reject)
         } catch (r) {
           reject(r)
         }
       })
 
-      that.rejectedCb.push(() => {
+      that.rejectedCb.push(function() {
         try {
-          const x = onRejected(that.value)
+          var x = onRejected(that.value)
           resolutionProcedure(promise2, x, resolve, reject)
         } catch (r) {
           reject(r)
@@ -124,10 +122,10 @@ MyPromise.prototype.then = function (onFulfilled, onRejected) {
     }))
   }
   if (that.state === RESOLVED) {
-    return (promise2 = new MyPromise((resolve, reject) => {
-      nextMacroTask(() => {
+    return (promise2 = new MyPromise(function(resolve, reject) {
+      nextMacroTask(function() {
         try {
-          const x = onFulfilled(that.value)
+          var x = onFulfilled(that.value)
           resolutionProcedure(promise2, x, resolve, reject)
         } catch (r) {
           reject(r)
@@ -136,10 +134,10 @@ MyPromise.prototype.then = function (onFulfilled, onRejected) {
     }))
   }
   if (that.state === REJECTED) {
-    return (promise2 = new MyPromise((resolve, reject) => {
-      nextMacroTask(() => {
+    return (promise2 = new MyPromise(function(resolve, reject) {
+      nextMacroTask(function() {
         try {
-          const x = onRejected(that.value)
+          var x = onRejected(that.value)
           resolutionProcedure(promise2, x, resolve, reject)
         } catch (r) {
           reject(r)
@@ -156,21 +154,21 @@ MyPromise.prototype.catch = function(onRejected) {
 MyPromise.all = function(promises) {
   if (!(promises instanceof Array)) throw new Error('bad array')
   if (promises.length === 0) {
-    return new MyPromise((resolve) => { resolve() })
+    return new MyPromise(function(resolve) { resolve() })
   }
 
-  return new MyPromise((resolve, reject) => {
+  return new MyPromise(function(resolve, reject) {
     var count = 0
     var result = []
-    promises.forEach((p, id) => {
+    promises.forEach(function(p, id) {
       if (p instanceof MyPromise) {
-        p.then(x => {
+        p.then(function(x) {
           count += 1
           result[id] = x
           if (count === promises.length) {
             resolve(result)
           }
-        }, e => {
+        }, function(e) {
           reject(e)
         })
       } else {
